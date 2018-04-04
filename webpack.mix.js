6e7333b85dc7c9f69230ac74d408441c1ed894e8
Marek Gebka <marek.gebka@ion2s.com>
2018-04-04 08:48:21 +0200

    Web-igniter magic added

---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+let mix = require('laravel-mix');
+
+mix.setPublicPath('public');
+
+mix.sass('src/sass/styles.scss', 'public/css')
+  .js('src/js/main.js', 'public/js')
+  .copy('src/index.html', 'public/index.html');
+
+
+mix.browserSync({"serveStatic":["./public"],"files":["./public"]});
